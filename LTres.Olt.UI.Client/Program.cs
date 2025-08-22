0fe01b1c4fb2a5bb394f948c7b85e003a78fe603
Luciano Tres <lucianotres@outlook.com>
2025-08-22 15:06:24 -0300

    Applied MudBlazor and adapted mainlayout

--- LTres.Olt.UI.Client/Program.cs
+++ LTres.Olt.UI.Client/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using MudBlazor.Services;
 using System.Net.Http.Headers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,6 +12,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddLocalization();
+builder.Services.AddMudServices();
 
 //authorization scheme services
 builder.Services.AddAuthorizationCore();
