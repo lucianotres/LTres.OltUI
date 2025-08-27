4e363a7dd44b7effd5f049fe187f3509bd51b5aa
Luciano Tres <lucianotres@outlook.com>
2025-08-27 16:17:19 -0300

    Include the model and validation at device page

--- LTres.Olt.UI.Shared/Models/OLT_Host.cs
+++ LTres.Olt.UI.Shared/Models/OLT_Host.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace LTres.Olt.UI.Shared.Models;
 
 public class OLT_Host
 {
     public Guid Id { get; set; }
+    [Required]
     public string Name { get; set; } = string.Empty;
+    [Required]
+    [RegularExpression(@".+:\d+", ErrorMessage = "Should be a valid DNS name or IP address")]
     public string Host { get; set; } = string.Empty;
     public string? SnmpCommunity { get; set; }
     public bool? SnmpBulk { get; set; }
@@ -12,8 +18,23 @@ public class OLT_Host
     public int Interface { get; set; }
     public bool? Active { get; set; }
 
-    
+
     public IEnumerable<string>? tags { get; set; }
     public OLT_Host_OnuRef? OnuRef { get; set; }
     public OLT_Host_CLIconfig? CLI { get; set; }
+
+    [JsonIgnore]
+    public string HostName
+    {
+        get => string.IsNullOrWhiteSpace(Host) ? string.Empty : Host.Split(':')[0];
+        set => Host = $"{value}:{HostPort}";
+    }
+
+    [JsonIgnore]
+    public int HostPort
+    {
+        get => string.IsNullOrWhiteSpace(Host) ? 0 :
+            int.TryParse(Host.Split(':')[1], out int port) ? port : 0;
+        set => Host = $"{HostName}:{value}";
+    }
 }
