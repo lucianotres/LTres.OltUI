49b2be1889696a33fa82d80fd5702aff2ec40efb
Luciano Tres <lucianotres@outlook.com>
2025-08-27 22:06:28 -0300

    Implemented the new device at device page

--- LTres.Olt.UI.Client/Services/OLTServices.cs
+++ LTres.Olt.UI.Client/Services/OLTServices.cs
@@ -5,20 +5,33 @@ namespace LTres.Olt.UI.Client.Services;
 
 public class OLTServices(IHttpClientFactory clientFactory)
 {
-	private readonly HttpClient _client = clientFactory.CreateClient("api");
+    private readonly HttpClient _client = clientFactory.CreateClient("api");
 
-	public async Task<IEnumerable<OLT_Host>?> GetHosts()
-	  => await _client.GetFromJsonAsync<IEnumerable<OLT_Host>>("OLTHost");
+    public async Task<IEnumerable<OLT_Host>?> GetHosts()
+        => await _client.GetFromJsonAsync<IEnumerable<OLT_Host>>("OLTHost");
 
-	public async Task<OLT_Host?> GetHost(Guid id)
-	  => await _client.GetFromJsonAsync<OLT_Host>($"OLTHost/{id}");
+    public async Task<OLT_Host?> GetHost(Guid id)
+    {
+        var result = await _client.GetAsync($"OLTHost/{id}");
+        return result.StatusCode == System.Net.HttpStatusCode.OK ?
+            await result.Content.ReadFromJsonAsync<OLT_Host>() :
+        null;
+    }
 
-	public async Task<Guid?> PutHost(OLT_Host host)
-	{
-		var result = await _client.PutAsJsonAsync($"OLTHost", host);
-		return result.IsSuccessStatusCode ?
-			await result.Content.ReadFromJsonAsync<Guid?>() :
-			null;
-	}
+    public async Task<Guid?> PostHost(OLT_Host host)
+    {
+        var result = await _client.PostAsJsonAsync($"OLTHost", host);
+        return result.IsSuccessStatusCode ?
+            await result.Content.ReadFromJsonAsync<Guid?>() :
+            null;
+    }
 
+    public async Task<Guid?> PutHost(OLT_Host host)
+    {
+        var result = await _client.PutAsJsonAsync($"OLTHost", host);
+        return result.IsSuccessStatusCode ?
+            await result.Content.ReadFromJsonAsync<Guid?>() :
+            null;
+    }
+    
 }
