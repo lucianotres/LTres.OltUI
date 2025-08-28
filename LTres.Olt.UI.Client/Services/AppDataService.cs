117524311f1a8998acd092f3079de68e9807f003
Luciano Tres <lucianotres@outlook.com>
2025-08-27 23:25:08 -0300

    New way of handle page titles

--- LTres.Olt.UI.Client/Services/AppDataService.cs
+++ LTres.Olt.UI.Client/Services/AppDataService.cs
@@ -1,9 +1,12 @@
 using System.ComponentModel;
+using Microsoft.JSInterop;
 
 namespace LTres.Olt.UI.Client.Services;
 
-public class AppDataService : INotifyPropertyChanged
+public class AppDataService(IJSRuntime js) : INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
     public bool Loading
     {
         get => _Loading;
@@ -15,5 +18,33 @@ public class AppDataService : INotifyPropertyChanged
     }
     private bool _Loading = false;
 
-    public event PropertyChangedEventHandler? PropertyChanged;
+    public string? Title { get; private set; }
+    public string? Description { get; private set; }
+ 
+    private void SetTitleInternal(string? title, string? description = null)
+    {
+        Title = title;
+        Description = description;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+    }
+
+    private async void SetAppTitle(string? appTitle)
+    {
+        await js.InvokeVoidAsync("eval", $"document.title = '{appTitle}'");        
+    }
+
+    public void SetTitle(string? title, string? description = null)
+    {
+        SetTitleInternal(title, description);
+        SetAppTitle(title);
+    }
+
+    public void SetTitle<T>(AppPageLocalizer<T> localizer)
+    {
+        var localizedDesc = localizer["Description"];
+        SetTitleInternal(localizer["Title"], localizedDesc.ResourceNotFound ? null : localizedDesc.Value);
+
+        SetAppTitle(localizer.GetTitle());
+    }
+
 }
